bcf2af075981f7c93c9e6eea90b9207e3a13820c
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-13 02:23:25 +0300

    Menü arama özelliği eklendi

--- src/app/layout/search/search.component.ts
+++ src/app/layout/search/search.component.ts
@@ -1,11 +1,68 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
+  search: string = "";
 
+  constructor(private router: Router) { }
+
+  onSearch() {
+    const rawInput = this.search.trim().toLowerCase();
+
+    const normalized = rawInput.replace(/[\s-]+/g, '');
+
+    const routeMap: { [key: string]: string} = {
+      'bank': '/banks',
+      'banks': '/banks',
+      'banka': '/banks',
+      'bankalar': '/banks',
+
+      'company': '/companies',
+      'companies': '/companies',
+      'şirket': '/companies',
+      'şirketler': '/companies',
+      'sirket': '/companies',
+      'sirketler': '/companies',
+
+      'customer': '/customers',
+      'customers': '/customers',
+      'cari': '/customers',
+      'cariler': '/customers',
+
+      'invoice': '/invoices',
+      'invoices': '/invoices',
+      'fatura': '/invoices',
+      'faturalar': '/invoices',
+
+      'product': '/products',
+      'products': '/products',
+      'ürün': '/products',
+      'ürünler': '/products',
+
+      'user': '/users',
+      'users': '/users',
+      'kullanici': '/users',
+      'kullanıcı': '/users',
+      'kullanıcılar': '/users',
+
+      'cashregister': '/cash-registers',
+      'cashregisters': '/cash-registers',
+      'kasa': '/cash-registers',
+      'kasalar': '/cash-registers'
+    };
+
+    const route = routeMap[normalized];
+    if(route) {
+      this.router.navigate([route]);
+    } else {
+      alert('Aradığınız sayfa bulunamadı');
+    }
+  }
 }
